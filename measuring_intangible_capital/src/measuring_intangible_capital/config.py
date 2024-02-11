755a323865f0f4eb7e0df0795b82b9c972bb5215
shtopane <vechesumstudent@gmail.com>
2024-02-11 15:11:01 +0100

    Added config and packages.

--- measuring_intangible_capital/src/measuring_intangible_capital/config.py
+++ measuring_intangible_capital/src/measuring_intangible_capital/config.py
@@ -4,9 +4,13 @@ from pathlib import Path
 SRC = Path(__file__).parent.resolve()
 BLD = SRC.joinpath("..", "..", "bld").resolve()
 
+DATA = SRC.joinpath("..", "..", "data").resolve()
+EU_KLEMS_WEBSITE = "https://euklems-intanprod-llee.luiss.it/download/"
+
 TEST_DIR = SRC.joinpath("..", "..", "tests").resolve()
 PAPER_DIR = SRC.joinpath("..", "..", "paper").resolve()
 
-GROUPS = ["marital_status", "qualification"]
+# EL is Greece
+COUNTRY_CODES = ["AT", "CZ", "DK", "EL", "SK"]
 
-__all__ = ["BLD", "SRC", "TEST_DIR", "GROUPS"]
+__all__ = ["BLD", "SRC", "TEST_DIR"]

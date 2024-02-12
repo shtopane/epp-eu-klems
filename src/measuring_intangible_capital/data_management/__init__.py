30333dfa71b7949b17d2f8e717ca9159d83205aa
shtopane <vechesumstudent@gmail.com>
2024-02-12 19:46:47 +0100

    Clean up. Added comments

--- src/measuring_intangible_capital/data_management/__init__.py
+++ src/measuring_intangible_capital/data_management/__init__.py
@@ -1,6 +1,9 @@
 """Functions for managing data."""
 
 from measuring_intangible_capital.data_management.clean_data import clean_data
-from measuring_intangible_capital.data_management.download import eu_klems_download
+from measuring_intangible_capital.data_management.download.eu_klems_download import (
+    get_eu_klems_download_page,
+    get_urls_file_names_by_country,
+)
 
-__all__ = [clean_data, eu_klems_download]
+__all__ = [clean_data, get_eu_klems_download_page, get_urls_file_names_by_country]
--- src/measuring_intangible_capital/data_management/download/task_eu_klems_download.py
+++ src/measuring_intangible_capital/data_management/download/task_eu_klems_download.py
@@ -1,37 +1,35 @@
 from pathlib import Path
 import urllib
-from typing import Annotated
 from bs4 import BeautifulSoup
-from pytask import task, Product
+from pytask import task
 
-from measuring_intangible_capital.data_management.download import eu_klems_download
+from measuring_intangible_capital.data_management.download.eu_klems_download import (
+    get_eu_klems_download_page,
+    get_urls_file_names_by_country,
+)
 from measuring_intangible_capital.config import (
     COUNTRY_CODES,
     EU_KLEMS_DATA_DOWNLOAD_PATH,
 )
 
 eu_klems_download_deps = {"scripts": Path("eu_klems_download.py")}
+eu_klems_download_page: BeautifulSoup = get_eu_klems_download_page()
 
 for country_code in COUNTRY_CODES:
 
     @task(id=country_code)
     def task_eu_klems_download(
+        page=eu_klems_download_page,
         country_code=country_code,
-        depends_on=eu_klems_download_deps
-    ):
-        """Download the EU KLEMS data."""
-        page: BeautifulSoup = None
+        depends_on=eu_klems_download_deps,
+    ) -> None:
+        """Download the EU KLEMS data for selected countries.
+        Gather links with country specific data account and download the file.
+        """
 
-        if page is None:
-            page = eu_klems_download.get_eu_klems_download_page()
+        urls, file_names = get_urls_file_names_by_country(page, country_code)
 
-        urls, file_names = eu_klems_download.get_urls_file_names_by_country(
-            page, country_code
-        )
-
-        for index, file_name in enumerate(file_names):
-            url_of_file = urls[index]
-
+        for url, file_name in zip(urls, file_names):
             urllib.request.urlretrieve(
-                url_of_file, EU_KLEMS_DATA_DOWNLOAD_PATH / country_code / file_name
+                url, EU_KLEMS_DATA_DOWNLOAD_PATH / country_code / file_name
             )

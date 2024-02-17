744131311072f7acea9a43051b7d94d72540bdf6
shtopane <vechesumstudent@gmail.com>
2024-02-17 15:09:40 +0100

    Comment all the file contents.

--- src/measuring_intangible_capital/analysis/task_analysis.py
+++ src/measuring_intangible_capital/analysis/task_analysis.py
@@ -1,47 +1,47 @@
-"""Tasks running the core analyses."""
+# """Tasks running the core analyses."""
 
-from pathlib import Path
+# from pathlib import Path
 
-import pandas as pd
-import pytask
+# import pandas as pd
+# import pytask
 
-from measuring_intangible_capital.analysis.model import fit_logit_model, load_model
-from measuring_intangible_capital.analysis.predict import predict_prob_by_age
-from measuring_intangible_capital.config import BLD, SRC
-from measuring_intangible_capital.utilities import read_yaml
+# from measuring_intangible_capital.analysis.model import fit_logit_model, load_model
+# from measuring_intangible_capital.analysis.predict import predict_prob_by_age
+# from measuring_intangible_capital.config import BLD, SRC
+# from measuring_intangible_capital.utilities import read_yaml
 
-fit_model_deps = {
-    "scripts": [Path("model.py"), Path("predict.py")],
-    "data": BLD / "python" / "data" / "data_clean.csv",
-    "data_info": SRC / "data_management" / "data_info.yaml",
-}
-
-
-def task_fit_model_python(
-    depends_on=fit_model_deps,
-    produces=BLD / "python" / "models" / "model.pickle",
-):
-    """Fit a logistic regression model (Python version)."""
-    data_info = read_yaml(depends_on["data_info"])
-    data = pd.read_csv(depends_on["data"])
-    model = fit_logit_model(data, data_info, model_type="linear")
-    model.save(produces)
+# fit_model_deps = {
+#     "scripts": [Path("model.py"), Path("predict.py")],
+#     "data": BLD / "python" / "data" / "data_clean.csv",
+#     "data_info": SRC / "data_management" / "data_info.yaml",
+# }
 
 
-# for group in GROUPS:
-#     predict_deps = {
-#         "data": BLD / "python" / "data" / "data_clean.csv",
-#         "model": BLD / "python" / "models" / "model.pickle",
-#     }
+# def task_fit_model_python(
+#     depends_on=fit_model_deps,
+#     produces=BLD / "python" / "models" / "model.pickle",
+# ):
+#     """Fit a logistic regression model (Python version)."""
+#     data_info = read_yaml(depends_on["data_info"])
+#     data = pd.read_csv(depends_on["data"])
+#     model = fit_logit_model(data, data_info, model_type="linear")
+#     model.save(produces)
 
-#     @pytask.task(id=group)
-#     def task_predict_python(
-#         group=group,
-#         depends_on=predict_deps,
-#         produces=BLD / "python" / "predictions" / f"{group}.csv",
-#     ):
-#         """Predict based on the model estimates (Python version)."""
-#         model = load_model(depends_on["model"])
-#         data = pd.read_csv(depends_on["data"])
-#         predicted_prob = predict_prob_by_age(data, model, group)
-#         predicted_prob.to_csv(produces, index=False)
+
+# # for group in GROUPS:
+# #     predict_deps = {
+# #         "data": BLD / "python" / "data" / "data_clean.csv",
+# #         "model": BLD / "python" / "models" / "model.pickle",
+# #     }
+
+# #     @pytask.task(id=group)
+# #     def task_predict_python(
+# #         group=group,
+# #         depends_on=predict_deps,
+# #         produces=BLD / "python" / "predictions" / f"{group}.csv",
+# #     ):
+# #         """Predict based on the model estimates (Python version)."""
+# #         model = load_model(depends_on["model"])
+# #         data = pd.read_csv(depends_on["data"])
+# #         predicted_prob = predict_prob_by_age(data, model, group)
+# #         predicted_prob.to_csv(produces, index=False)

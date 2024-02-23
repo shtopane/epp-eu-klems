9dfda73126dadfe80d8b543bb527d8defc02baa1
shtopane <vechesumstudent@gmail.com>
2024-02-23 19:12:17 +0100

    Clean up and add comments in task functions

--- src/measuring_intangible_capital/analysis/task_share_intangible_of_gdp.py
+++ src/measuring_intangible_capital/analysis/task_share_intangible_of_gdp.py
@@ -16,15 +16,22 @@ def task_share_intangible_of_gdp(
   depends_on=share_intangible_of_gdp_deps,
   path_to_shares_intangible = BLD / "python" / "share_intangible" / "shares_intangible_of_gdp.pkl"
 ):
-  """Calculate share of intangible investment of GDP for a country."""
+  """Calculate share of intangible investment of GDP for a country from 1995 until 2006.
+  For each country, select the capital and national accounts data 
+  for the years 1995 to 2006 and get the total values for the whole economy(all industries).
+  Then calculate the share of intangible investment of GDP for each year and country.
+  Store the data frame for each country and concatenate at the end.
+  The result is a data frame for all countries with columns: year, country, share_intangible, intangible_gdp.
+  Lastly, save the data frame to a pickle file.
+  """
   years = range(1995, 2007)
-  dfs_merged = []
+  dfs = []
 
   for country_code in COUNTRY_CODES:
     capital_accounts_for_years, national_accounts_for_years = get_country_total_gdp_investment(country_code, years)
-    data_merged = get_share_of_intangible_investment_per_gdp(capital_accounts_for_years, national_accounts_for_years)
-    dfs_merged.append(data_merged)
+    df = get_share_of_intangible_investment_per_gdp(capital_accounts_for_years, national_accounts_for_years)
+    dfs.append(df)
   
-  data_for_plotting = pd.concat(dfs_merged)
+  data_for_plotting = pd.concat(dfs)
 
   pd.to_pickle(data_for_plotting, path_to_shares_intangible)
--- src/measuring_intangible_capital/final/task_plot_share_intangible_of_gdp.py
+++ src/measuring_intangible_capital/final/task_plot_share_intangible_of_gdp.py
@@ -12,7 +12,9 @@ plot_share_intangible_of_gdp_deps = {
 }
 
 def task_plot_share_intangible_of_gdp(depends_on = plot_share_intangible_of_gdp_deps, plot_save_path = BLD / "figures" / "shares_intangible_investment_gdp.png"):
-    """Calculate share of intangible investment of GDP for a country."""
+    """Plot share of intangible investment as percent of GDP for all years and countries.
+    Save the plot as png file in the figures folder.
+    """
     df = pd.read_pickle(depends_on["data"])
     fig = plot_share_intangibles_all_countries(df)
     fig.write_image(plot_save_path)

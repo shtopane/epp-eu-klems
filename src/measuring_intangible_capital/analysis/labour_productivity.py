f24f695e1ccec381ce9c7448110311c63899922f
shtopane <vechesumstudent@gmail.com>
2024-03-06 15:43:10 +0100

    Simplify function. Rename and clean up.

--- src/measuring_intangible_capital/analysis/labour_productivity.py
+++ src/measuring_intangible_capital/analysis/labour_productivity.py
@@ -1,21 +1,20 @@
 """Functions for calculating labour productivity."""
 import pandas as pd
+import numpy as np
 
 from measuring_intangible_capital.config import INTANGIBLE_AGGREGATE_CATEGORIES
 
-def get_share_of_intangible_sub_components_in_labour_productivity(df: pd.DataFrame, percentages: list[float]):
+def get_share_of_intangible_sub_components_in_labour_productivity(intangible_composition: np.float64, percentages: list[float]):
     """Calculate the share of intangible sub-components in labour productivity.
     
     Args:
-        df (pd.DataFrame): The data frame with the composition of labour productivity growth. For a given country.
+        intangible_composition (np.float64): the share of intangible capital deepening in labour productivity growth. For a given country.
         percentages (dict): The percentages of intangible sub-components in labour productivity. For a given country.
     Returns:
         pd.Series: The data frame with the share of intangible sub-components in labour productivity.
     """
     sr = pd.Series()
-    intangible_component = df["intangible"]
-
-    intangible_sub_components = [round(intangible_component * percentage, 3) for percentage in percentages]
+    intangible_sub_components = [round(intangible_composition * percentage, 3) for percentage in percentages]
 
     for index, column in enumerate(INTANGIBLE_AGGREGATE_CATEGORIES):
         sr[column] = intangible_sub_components[index]
--- src/measuring_intangible_capital/analysis/task_intangible_components_labour_productivity_composition.py
+++ src/measuring_intangible_capital/analysis/task_intangible_components_labour_productivity_composition.py
@@ -1,38 +1,50 @@
-
 from pathlib import Path
-
 import pandas as pd
-from measuring_intangible_capital.analysis.labour_productivity import get_share_of_intangible_sub_components_in_labour_productivity
 
-from measuring_intangible_capital.config import BLD_PYTHON, COUNTRY_CODES_LESS_SK, DATA_CLEAN_PATH, INTANGIBLE_AGGREGATE_CATEGORIES
-from measuring_intangible_capital.utilities import get_percent_of_intangible_sub_components_in_labour_productivity
+from measuring_intangible_capital.analysis.labour_productivity import (
+    get_share_of_intangible_sub_components_in_labour_productivity,
+)
+
+from measuring_intangible_capital.config import (
+    BLD_PYTHON,
+    COUNTRY_CODES_LESS_SK,
+    INTANGIBLE_AGGREGATE_CATEGORIES,
+)
+from measuring_intangible_capital.utilities import (
+    get_percent_of_intangible_sub_components_in_labour_productivity,
+)
 
 
 intangible_components_labour_productivity_composition_deps = {
-  "scripts": [Path("labour_productivity.py")],
-  "labour_composition": Path(BLD_PYTHON / "labour_productivity" / "composition.pkl"),
-
+    "scripts": [Path("labour_productivity.py")],
+    "data": Path(BLD_PYTHON / "labour_productivity" / "composition.pkl"),
 }
+
+
 def task_intangible_components_labour_productivity_composition(
     depends_on=intangible_components_labour_productivity_composition_deps,
-    path_to_intangible_components_labour_productivity_composition: Path = BLD_PYTHON / "labour_productivity" / "intangible_composition.pkl"
+    path_to_data: Path = BLD_PYTHON / "labour_productivity" / "intangible_composition.pkl",
 ):
-  labour_composition = pd.read_pickle(depends_on["labour_composition"])
-  
-  df = pd.DataFrame(index=labour_composition.index, columns=INTANGIBLE_AGGREGATE_CATEGORIES)
+    labour_productivity_composition = pd.read_pickle(depends_on["data"])
 
-  for country_code in COUNTRY_CODES_LESS_SK:
-    labour_composition_for_country = labour_composition.loc[country_code]
-    
-    percentages = get_percent_of_intangible_sub_components_in_labour_productivity()
-    percentages_for_country = percentages[country_code]
-
-    sub_components = get_share_of_intangible_sub_components_in_labour_productivity(
-        labour_composition_for_country,
-        percentages_for_country
+    df = pd.DataFrame(
+        index=labour_productivity_composition.index,
+        columns=INTANGIBLE_AGGREGATE_CATEGORIES,
     )
-    
-    df.loc[country_code, :] = sub_components
-  
-  
-  pd.to_pickle(df, path_to_intangible_components_labour_productivity_composition)
+
+    for country_code in COUNTRY_CODES_LESS_SK:
+        labour_composition_for_country = labour_productivity_composition.loc[
+            country_code
+        ]
+        intangible_composition = labour_composition_for_country["intangible"]
+
+        percentages = get_percent_of_intangible_sub_components_in_labour_productivity()
+        percentages_for_country = percentages[country_code]
+
+        sub_components = get_share_of_intangible_sub_components_in_labour_productivity(
+            intangible_composition, percentages_for_country
+        )
+
+        df.loc[country_code, :] = sub_components
+
+    pd.to_pickle(df, path_to_data)
